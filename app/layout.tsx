ffcec49339c1305c3e744609385cb2957ddccb1c
kain <kainpro@foxmail.com>
2025-11-03 16:27:22 +0800

    update: 路由

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { App, ConfigProvider, Layout } from 'antd';
+import zhCN from 'antd/locale/zh_CN';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-});
 
 export const metadata: Metadata = {
   title: 'Cronx-Admin'
@@ -22,8 +14,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="zh">
+      <body>
+        <AntdRegistry>
+          <ConfigProvider
+            locale={zhCN}
+            theme={{
+              components: {
+                Layout: { headerBg: '#fff', headerPadding: '0 16px' }
+              }
+            }}
+          >
+            <App style={{ height: '100%' }}>
+              <Layout style={{ height: '100%' }}>{children}</Layout>
+            </App>
+          </ConfigProvider>
+        </AntdRegistry>
+      </body>
     </html>
   );
 }
