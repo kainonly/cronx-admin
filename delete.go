31026194d34a3d16412e92b31b271fbdbbdcb6d0
kain <kainpro@foxmail.com>
2025-12-27 14:33:16 +0800

    update

--- delete.go
+++
@@ -1,7 +0,0 @@
-package crud
-
-// DeleteDto represents the data transfer object for batch delete operations.
-type DeleteDto struct {
-	// IDs contains the list of resource identifiers to be deleted.
-	IDs []string `json:"ids"`
-}
